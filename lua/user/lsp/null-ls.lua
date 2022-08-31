305b877baafc5aca3cb61f83c0fe6638797fdb89
Blake Johnson <blakeejohnson39@gmail.com>
2022-08-31 13:26:17 -0500

    feat: add formatters and diagnostics

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -12,8 +12,10 @@ null_ls.setup({
 	debug = false,
 	sources = {
 		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-		formatting.black.with({ extra_args = { "--fast" } }),
+		formatting.black,
+    formatting.isort,
 		formatting.stylua,
-    -- diagnostics.flake8
+    diagnostics.flake8,
+    diagnostics.mypy,
 	},
 })
